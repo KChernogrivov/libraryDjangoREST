49bac4432930f7718f41e17e7479676858a2bb3d
Kirill Chernogrivov <kirill.2201@inbox.ru>
2022-11-17 09:47:41 +0700

    added models/api for books and authors

---
+++ home/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('books/', views.book_list),
+    path('books/<int:pk>/', views.book_detail),
+
+    path('authors/', views.author_list),
+    path('authors/<int:pk>/', views.author_detail),
+]
---
+++ home/views.py
@@ -0,0 +1,99 @@
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from .models import Book, Author
+from .serializers import BookSerializer, AuthorSerializer
+
+
+@csrf_exempt
+def book_list(request):
+   """
+   List all code books, or create a new book.
+   """
+   if request.method == 'GET':
+       books = Book.objects.all()
+       serializer = BookSerializer(books, many=True)
+       return JsonResponse(serializer.data, safe=False)
+
+   elif request.method == 'POST':
+       data = JSONParser().parse(request)
+       serializer = BookSerializer(data=data)
+       if serializer.is_valid():
+           serializer.save()
+           return JsonResponse(serializer.data, status=201)
+       return JsonResponse(serializer.errors, status=400)
+
+
+@csrf_exempt
+def book_detail(request, pk):
+   """
+   Retrieve, update or delete a code book.
+   """
+   try:
+       book = Book.objects.get(pk=pk)
+   except Book.DoesNotExist:
+       return HttpResponse(status=404)
+
+   if request.method == 'GET':
+       serializer = BookSerializer(book)
+       return JsonResponse(serializer.data)
+
+   elif request.method == 'PUT':
+       data = JSONParser().parse(request)
+       serializer = BookSerializer(book, data=data)
+       if serializer.is_valid():
+           serializer.save()
+           return JsonResponse(serializer.data)
+       return JsonResponse(serializer.errors, status=400)
+
+   elif request.method == 'DELETE':
+       book.delete()
+       return HttpResponse(status=204)
+
+
+
+
+@csrf_exempt
+def author_list(request):
+   """
+   List all code books, or create a new book.
+   """
+   if request.method == 'GET':
+       authors = Author.objects.all()
+       serializer = AuthorSerializer(authors, many=True)
+       return JsonResponse(serializer.data, safe=False)
+
+   elif request.method == 'POST':
+       data = JSONParser().parse(request)
+       serializer = AuthorSerializer(data=data)
+       if serializer.is_valid():
+           serializer.save()
+           return JsonResponse(serializer.data, status=201)
+       return JsonResponse(serializer.errors, status=400)
+
+
+@csrf_exempt
+def author_detail(request, pk):
+   """
+   Retrieve, update or delete a code book.
+   """
+   try:
+       author = Book.objects.get(pk=pk)
+   except Book.DoesNotExist:
+       return HttpResponse(status=404)
+
+   if request.method == 'GET':
+       serializer = BookSerializer(author)
+       return JsonResponse(serializer.data)
+
+   elif request.method == 'PUT':
+       data = JSONParser().parse(request)
+       serializer = BookSerializer(author, data=data)
+       if serializer.is_valid():
+           serializer.save()
+           return JsonResponse(serializer.data)
+       return JsonResponse(serializer.errors, status=400)
+
+   elif request.method == 'DELETE':
+       author.delete()
+       return HttpResponse(status=204)
